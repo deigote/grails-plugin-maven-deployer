4ee7eb5c297a3f82e3eb59fcfb40b2a968e03937
deigote <diego@toharia.com>
2013-06-01 20:30:20 +0200

    Initial commit with the first version

---
+++ deploy.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+SCRIPT_HOME="`pwd`/`dirname $0`"
+
+if [ "$#" -lt 4 ] ; then
+	echo "Arguments missing! Usage: `basename $0` <plugin-home> <group-id> <plugin-name> <releases_dir>"
+	exit 1
+else
+	PLUGIN_HOME="$1"
+	GROUP_ID="$2"
+	PLUGIN_NAME="$3"
+	DEST="$4"
+fi
+
+function findInPluginDescription() {
+	RES=$(cat *GrailsPlugin.groovy | grep "def $1 = " | cut -d"'" -f2)
+	echo "$RES" | grep "def $1 = " &> /dev/null && RES=$(cat *GrailsPlugin.groovy | grep "def $1 = " | cut -d'"' -f2)
+	echo "$RES"
+}
+
+cd "$PLUGIN_HOME"
+PLUGIN_VERSION=$(findInPluginDescription version)
+DESCRIPTION=$(findInPluginDescription description)
+JAR_FILENAME=grails-plugin-"$PLUGIN_NAME"-"$PLUGIN_VERSION".jar 
+SCRIPT_TMP="$SCRIPT_HOME/tmp.$$"
+DEST_FROM_GROUP="$(echo $GROUP_ID | tr '.' '/' )"
+
+echo "Compiling and building plugin..."
+grails clean && grails compile && grails package-plugin --binary
+if [ ! -f "target/$JAR_FILENAME" ] ; then
+	echo "target/$JAR_FILENAME not found after building binary plugin, aborting..."
+	exit 1
+fi
+
+cd "$SCRIPT_HOME"
+mkdir "$SCRIPT_TMP"
+cd "$SCRIPT_TMP"
+
+echo "Generating pom from template..."
+cat ../template.pom | sed "s/_GROUP_ID_/$GROUP_ID/" | \
+	sed "s/_NAME_/$PLUGIN_NAME/" | \
+	sed "s/_VERSION_/$PLUGIN_VERSION/" | \
+	sed "s/_DESCRIPTION_/$DESCRIPTION/" > $PLUGIN_NAME.pom
+
+echo "Installing version $GROUP_ID:$PLUGIN_NAME:$PLUGIN_VERSION with description '$DESCRIPTION'"
+
+mvn install:install-file -Dmaven.repo.local="." -Dfile="$PLUGIN_HOME/target/$JAR_FILENAME" -DgroupId="$GROUP_ID" \
+	-DartifactId="$PLUGIN_NAME" -Dversion="$PLUGIN_VERSION" -DpomFile="$PLUGIN_NAME.pom" -Dpackaging=jar -DcreateChecksum=true
+
+echo "Moving $DEST_FROM_GROUP/$PLUGIN_NAME to $DEST"
+
+mkdir -p $DEST/$DEST_FROM_GROUP
+cp -R $DEST_FROM_GROUP/$PLUGIN_NAME $DEST/$DEST_FROM_GROUP
+cd ..
+rm -r "$SCRIPT_TMP" 
